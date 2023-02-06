c449b721b38268229e87d277501be06f0f21a2a1
Kate Modler <kmodler@unc.edu>
2023-02-05 23:04:23 -0500

    one shot wordle complete

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -9,25 +9,31 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 letter_idx: int = 0
-letter_anywhere: bool = False
-all_letters: int = 0
+
 result: str = "" 
 
-# make sure your word is 6 letters
+# make sure word is the right amount of letters
 while (len(word_guess) != len(secret_word)):
     word_guess: str = input(f"That was not {len(secret_word)} Letters! Try again: ")
 
+# while loop to check all letters
 while (letter_idx < len(secret_word)):
+    letter_anywhere: bool = False
+    compare_idx: int = 0
+    # if the letter is in the right spot as the secret word print a green box
     if(secret_word[letter_idx] == word_guess[letter_idx]):
         result = result + GREEN_BOX
-        while (letter_anywhere and letter_idx < len(secret_word)):
-            if (all_letters == word_guess[letter_idx]):
-                letter_anywhere = True
-                result = result + YELLOW_BOX    
-            else:
-                letter_idx = letter_idx + 1 
     else:
-        result = result + WHITE_BOX
+        # check if the letter is anywhere in the word
+        while (not letter_anywhere and compare_idx < len(secret_word)):
+            if (secret_word[compare_idx] == word_guess[letter_idx]):
+                letter_anywhere = True
+            else:
+                compare_idx = compare_idx + 1 
+        if letter_anywhere == True:
+            result = result + YELLOW_BOX
+        else:
+            result = result + WHITE_BOX  
     letter_idx = letter_idx + 1    
 
 print(result)  
@@ -37,4 +43,3 @@ if (word_guess == secret_word):
      print("Woo! You got it! ")
 else:
     print("Not quite. Play again soon! ")
-
