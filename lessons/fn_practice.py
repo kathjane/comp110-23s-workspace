1a3212d5fc9f84f5bdeb7207a67c30ebcca2d6ed
Kate Modler <kmodler@unc.edu>
2023-02-07 20:51:42 -0500

    lessons 9,10

---
+++ lessons/fn_practice.py
@@ -0,0 +1,7 @@
+"""Example functions to learn definitions and calling syntax"""
+
+def my_max(number1: int, number2: int) -> int:
+    """Returns the maximum value out of two numbers"""
+    if number1 >= number2:
+        return number1
+        
