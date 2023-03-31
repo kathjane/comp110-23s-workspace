9fb2a97b1fdbb3958d9d2f3f5906f8df8863653e
Kate Modler <kmodler@unc.edu>
2023-03-30 23:15:27 -0400

    dict update

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -1,10 +1,11 @@
-"""EX07: Dictionary"""
+"""EX07: Dictionary!"""
 
 __author__ = "730486658"
 
-def invert(input_dict: dict[str,str]) -> dict[str,str]:
-    """Given a dictionary of strings it will invert it so the keys become values and values become keys"""
-    result: dict[str,str] = dict()
+
+def invert(input_dict: dict[str, str]) -> dict[str, str]:
+    """Given a dictionary of strings it will invert it so the keys become values and values become keys."""
+    result: dict[str, str] = dict()
     value_list: list[str] = []
     key_list: list[str] = []
     
@@ -23,29 +24,29 @@ def invert(input_dict: dict[str,str]) -> dict[str,str]:
     repeat: bool = False
     list_idx1: int = 0
     list_idx2: int = 0
-    while repeat == False and list_idx2 < len(value_list):
+    while repeat is False and list_idx2 < len(value_list):
         list_idx1 = list_idx2 + 1
-        while repeat == False and list_idx1 < len(value_list):
+        while repeat is False and list_idx1 < len(value_list):
             if value_list[list_idx1] == value_list[list_idx2]:
                 repeat = True
             else: 
                 list_idx1 += 1
         list_idx2 += 1
     
-    if repeat == True:
+    if repeat is True:
         raise KeyError("List cannot be inverted because keys cannot be repeated!")
 
     return result
     
 
-def favorite_color(input_dict: dict[str,str]) -> str:
-    """Takes a dictionary of people's favorite color and returns the color that has the most counts"""
+def favorite_color(input_dict: dict[str, str]) -> str:
+    """Takes a dictionary of people's favorite color and returns the color that has the most counts."""
     color_list: list[str] = []
     for key in input_dict:
         color_list.append(input_dict[key])
     
     idx = 0
-    color_count: dict[str,int] = dict()
+    color_count: dict[str, int] = dict()
     while idx < len(color_list):
         if color_list[idx] in color_count:
             color_count[color_list[idx]] += 1
@@ -53,7 +54,6 @@ def favorite_color(input_dict: dict[str,str]) -> str:
         else:
             color_count[color_list[idx]] = 1
             idx += 1
-    
     
     highest_v: int = 0
     for key, value in color_count.items():
@@ -65,8 +65,10 @@ def favorite_color(input_dict: dict[str,str]) -> str:
 
     return fav_color
 
+
 def count(input_list: list[str]) -> dict[str, int]:
-    result: dict[str,int] = dict()
+    """Counts the number each time a str occurs in a given list and returns a dictionary of the counts."""
+    result: dict[str, int] = dict()
     idx = 0
 
     while idx < len(input_list):
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -1,4 +1,4 @@
-"""EX07: Unit tests for Dictionary"""
+"""EX07: Unit tests for Dictionary!"""
 
 __author__ = "730486658"
 
@@ -8,51 +8,70 @@ from exercises.ex07.dictionary import favorite_color
 from exercises.ex07.dictionary import count
 
 # Invert Tests
+
+
 def test_invert1() -> None: 
-    dict1: dict[str,str] = {"name" : "Kate", "age" : "20", "fav color" : "green"}
-    result1: dict[str,str] = {"Kate" : "name", "20" : "age", "green" : "fav color"}
+    """Tests Invert ex 1."""
+    dict1: dict[str, str] = {"name": "Kate", "age": "20", "fav color": "green"}
+    result1: dict[str, str] = {"Kate": "name", "20": "age", "green": "fav color"}
     assert invert(dict1) == result1 
+
 
 def test_invert2() -> None: 
-    dict1: dict[str,str] = {"one" : "uno", "two" : "dos", "three" : "tres"}
-    result1: dict[str,str] = {"uno" : "one", "dos" : "two", "tres" : "three"}
+    """Tests Invert ex 2."""
+    dict1: dict[str, str] = {"one": "uno", "two": "dos", "three": "tres"}
+    result1: dict[str, str] = {"uno": "one", "dos": "two", "tres": "three"}
     assert invert(dict1) == result1 
 
+
 def test_invert3_edge() -> None:
+    """Tests Invert ex 3 (edge case)."""
     with pytest.raises(KeyError):
-        my_dict = {"110" : "A", "201" : "B", "231" : "A"}
+        my_dict = {"110": "A", "201": "B", "231": "A"}
         invert(my_dict)
 
 # Favorite Color Tests
 
+
 def test_fav_color1() -> None: 
-    input_dict: dict[str,str] = {"Marc" : "yellow", "Ezri" : "blue", "Kris" : "blue"}
+    """Tests Favorite Color ex 1."""
+    input_dict: dict[str, str] = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}
     result: str = "blue"
     assert favorite_color(input_dict) == result
 
+
 def test_fav_color2() -> None: 
-    input_dict: dict[str,str] = {"Marc" : "green", "Ezri" : "green", "Kris" : "blue"}
+    """Tests Favorite Color ex 2."""
+    input_dict: dict[str, str] = {"Marc": "green", "Ezri": "green", "Kris": "blue"}
     result: str = "green"
     assert favorite_color(input_dict) == result
 
+
 def test_fav_color3_edge() -> None: 
-    input_dict: dict[str,str] = {"Marc" : "green", "Ezri" : "yellow", "Kris" : "blue"}
+    """Tests Favorite Color ex 3 (edge case)."""
+    input_dict: dict[str, str] = {"Marc": "green", "Ezri": "yellow", "Kris": "blue"}
     result: str = "green"
     assert favorite_color(input_dict) == result
 
 # Count Tests
 
+
 def test_count1() -> None:
-    input_list: list[str] = ["h","a","p","p","y"]
-    result: dict[str,int] = {"h" : 1, "a" : 1, "p" : 2, "y" : 1}
+    """Tests Count ex 1."""
+    input_list: list[str] = ["h", "a", "p", "p", "y"]
+    result: dict[str, int] = {"h": 1, "a": 1, "p": 2, "y": 1}
     assert count(input_list) == result
+
 
 def test_count2() -> None:
-    input_list: list[str] = ["yes","no","yes","yes","no"]
-    result: dict[str,int] = {"yes" : 3, "no" : 2}
+    """Tests Count ex 2."""
+    input_list: list[str] = ["yes", "no", "yes", "yes", "no"]
+    result: dict[str, int] = {"yes": 3, "no": 2}
     assert count(input_list) == result
 
+
 def test_count3_edge() -> None:
+    """Tests Count ex 3 (edge case)."""
     input_list: list[str] = ()
-    result: dict[str,int] = dict()
+    result: dict[str, int] = dict()
     assert count(input_list) == result
